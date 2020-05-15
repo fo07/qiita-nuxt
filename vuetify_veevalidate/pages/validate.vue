e280b48dc598f6cbc491b9438f585d4fab02a50e
fo <fo.shuukatsu@gmail.com>
2020-05-15 11:23:34 +0900

    update: validation-providerの動作確認

--- vuetify_veevalidate/pages/validate.vue
+++ vuetify_veevalidate/pages/validate.vue
@@ -1,21 +1,15 @@
 <template>
-  <ValidationProvider rules="required" v-slot="{ errors }">
-    <v-text-field
-        v-model="name"
-        :error-messages="errors"
-        label="お名前"
-    />
-    <v-text-field
-        v-model="email"
-        :error-messages="errors"
-        label="メールアドレス"
-    />
-    <v-text-field
-        v-model="password"
-        :error-messages="errors"
-        label="パスワード"
-    />
-  </ValidationProvider>
+  <div>
+    <ValidationProvider rules="required" v-slot="{ errors }">
+      <v-text-field v-model="name" :error-messages="errors" label="お名前" />
+    </ValidationProvider>
+    <validation-provider>
+      <v-text-field v-model="email" :error-messages="errors" label="メールアドレス" />
+    </validation-provider>
+    <validation-provider>
+      <v-text-field v-model="password" :error-messages="errors" label="パスワード" />
+    </validation-provider>
+  </div>
 </template>
 
 <script>
@@ -29,7 +23,8 @@ export default {
     return {
       name: "",
       email: "",
-      password: ""
+      password: "",
+      errors: ""
     };
   }
 };
